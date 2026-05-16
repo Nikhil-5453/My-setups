1dd51732852f55dabed2c49982cc0d000cb8cb29
Nikhil <106131405+Nikhil-5453@users.noreply.github.com>
2026-05-16 21:09:03 +0530

    Fix installation script for awscli and KOPS

--- KOPS.sh
+++ KOPS.sh
@@ -3,7 +3,7 @@
 
 echo "--------Installing awscli on server----------"
 echo "Removing existing cli"
-sudo yum remove awscli
+sudo yum remove awscli -y
 
 echo "Installing latest version of aws-cli by curl"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -14,7 +14,8 @@ echo "modifying permissions"
 sudo +x aws
 
 echo "modify export in .bashrc"
-sed -i '& a\export PATH:$PATH:/usr/local/bin' .bashrc
+sed -i '$ a\export PATH=$PATH:/usr/local/bin' ~/.bashrc
+source ~/.bashrc
 
 echo "--------Installing KOPS---------"
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
@@ -26,10 +27,18 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
-echo "==========Checking intallation==========="
+echo "===================Checking intallation===================="
+echo "--------checking executable path---------"
+echo $PATH
+
+echo "-------------awscli version--------------"
 which aws
 aws --version
+
+echo "-------------KOPS version----------------"
 kops version
+
+echo "------------Kubectl version-------------"
 kubectl version --client
 
 echo "**===============Installtions successful=============**"
