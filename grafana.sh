90fa34a6b8b78da085470583fa470d6ad630e0af
Nikhil <106131405+Nikhil-5453@users.noreply.github.com>
2026-07-28 10:55:51 +0530

    Add installation script for Grafana using Helm
    This script installs Grafana using Helm charts, sets up the necessary namespace, and provides instructions for verifying the installation.

---
+++ grafana.sh
@@ -0,0 +1,22 @@
+# Install grafana from helm-charts
+
+# First install metric server as given in promethus.sh -> install helm -> install prometheus -> then Install GRAFANA.
+
+echo "**--------- Downloading Grafana repo from helm-chart ------------**"
+helm repo add grafana-repo https://grafana.github.io/helm-charts
+helm repo update
+helm repo list
+
+# Install Grsfana from repo.
+echo "**----------- Installing grafana from repo --------------**"
+kubectl create ns grafana-ns
+helm install grafana grafana-repo/grafana -n grafana-ns --set persistence.storageClassName=standard  --set persistence.enabled=true --set adminPassword='EKS!sAWSome' --set service.type=LoadBalncer
+
+echo " Here, grafana installed and service create on LB; if its on MINIKUBE cluster
+       don't forget to port-forward the NodePort
+       cmd: nohup kubectl port-forward -n grafana-ns svc/grafana 31898:80 --address 0.0.0.0 >> grafana-port.log 2>&1 &
+       this will keep running service background"
+
+echo "**------------- verify grafana resources -------------**"
+kubectl get all -n grafana-ns
+       
